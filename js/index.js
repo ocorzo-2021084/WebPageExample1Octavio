c8e8a15ef2151a4bea3281d100e9c71f9c00b5f1
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2023-01-20 09:21:52 -0600

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function sumar(){
+    let numA = document.getElementById("num1").value
+    let numB = document.getElementById("num2").value
+
+    let resultado = parseInt(numA) + parseInt(numB) 
+    console.log(resultado);
+}
